b846407f7c6b68702bdef1ee544966fa93d6e4e1
svpanchal <svpanchal@gmail.com>
2019-03-08 10:48:27 -0500

    Adding QA env; updating libraries; karma conf in progress

--- karma.conf.js
+++ karma.conf.js
@@ -3,20 +3,43 @@
 
 module.exports = function (config) {
   config.set({
+    preprocessors: {
+      '**/*.ts': ['karma-typescript']
+    },
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler
+      options: {
+        sourceMap: false, // (optional) Generates corresponding .map file.
+        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+        noResolve: true, // (optional) Skip resolution and preprocessing.
+        removeComments: true, // (optional) Do not emit comments to output.
+        concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+      },
+      // transforming the filenames
+      transformPath: function (path) {
+        return path.replace(/\.ts$/, '.js');
+      }
+    },
     basePath: '',
+    files: [
+      'src/app/*.ts'
+    ],
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-typescript')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
