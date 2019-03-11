d45021f247348ea6f4f0251e03ddc39b74371a28
RMyers <Rush.Myers@MatrixRes.com>
2019-03-11 11:20:54 -0400

    update @karma/types version and make change in karma config

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
   config.set({
     preprocessors: {
-      '**/*.ts': ['karma-typescript']
+      '**/*.tsx': ['karma-typescript']
     },
     typescriptPreprocessor: {
       // options passed to the typescript compiler
